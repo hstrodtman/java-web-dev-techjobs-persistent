21a8f703ec4517481da6a515c8c2d851f484e05f
hstrodtman <heatherstrodtman@gmail.com>
2020-03-11 20:39:04 -0500

    Added necessary dependencies, updated application.properties to connect to MySql database, updated Employer/Skills and the Abstract entity. Created the Employer & Skill Repositories and updated respective controllers to utilize them. Up to date to Part 2 Test it with SQL.

--- queries.sql
+++ queries.sql
@@ -1,5 +1,12 @@
 ## Part 1: Test it with SQL
 
+SHOW COLUMNS FROM job;
+
+id: int
+employer: varchar(255)
+name: varchar(255)
+skill: varchar(255)
+
 ## Part 2: Test it with SQL
 
 ## Part 3: Test it with SQL
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/EmployerController.java
@@ -1,6 +1,8 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
 import org.launchcode.javawebdevtechjobspersistent.models.Employer;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -13,9 +15,12 @@ import java.util.Optional;
 @RequestMapping("employers")
 public class EmployerController {
 
+    @Autowired
+    private EmployerRepository employerRepository;
 
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
+        model.addAttribute("title", "Add Employer");
         model.addAttribute(new Employer());
         return "employers/add";
     }
@@ -25,8 +30,10 @@ public class EmployerController {
                                     Errors errors, Model model) {
 
         if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Employer");
             return "employers/add";
         }
+           employerRepository.save(newEmployer);
 
         return "redirect:";
     }
@@ -34,7 +41,7 @@ public class EmployerController {
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
 
-        Optional optEmployer = null;
+        Optional<Employer> optEmployer = employerRepository.findById(employerId);
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
